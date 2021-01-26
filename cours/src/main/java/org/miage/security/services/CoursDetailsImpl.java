153c282f721c2fde02c6153777c27ea550f7120c
Anthony pellizzeri <anthony.pellizzeri3@orange.fr>
2021-01-26 11:47:54 +0100

    refonte

---
+++ cours/src/main/java/org/miage/security/services/CoursDetailsImpl.java
@@ -0,0 +1,103 @@
+package org.miage.security.services;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import org.miage.Entity.database.cours;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class CoursDetailsImpl implements UserDetails {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String username;
+
+	private String email;
+
+	@JsonIgnore
+	private String password;
+
+	private boolean admin;
+
+	public CoursDetailsImpl(String username, String email, String password,
+							boolean admin) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.admin = admin;
+	}
+
+	public static CoursDetailsImpl build(cours cours) {
+		return new CoursDetailsImpl(
+				"user.getUsername()",
+				"user.getEmail()",
+				"user.getPassword()",
+				true);
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		CoursDetailsImpl user = (CoursDetailsImpl) o;
+		return Objects.equals(id, user.id);
+	}
+}
